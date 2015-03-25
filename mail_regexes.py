92680995a2c68b3233582e109a2d6690bb9ca7b7
Andrew Tranquada <andrew.tranquada@mailtrust.com>
2015-03-25 07:22:17 -0400

    regex rename

--- mail_regexes.py
+++ mail_regexes.py
@@ -1,47 +1,48 @@
 import re
 
-get_originating_ip_re = re.compile(
+sending_ip_re = re.compile(
     "\w+\s+\d+\s[0-9:]+\s[\w\.].+ postfix\/smtpd\[\d+\]: (\S+): "
     "client=\S+\[(\S+)\]")
-get_subject_and_addrs_re = re.compile(
+subject_and_from_re = re.compile(
     "\w+\s+\d+\s[0-9:]+\s[\w\.].+ postfix\/cleanup\[\d+\]: (\S+): "
     "warning: header Subject: (.+) from .+ from=<(\S+)> to=<(\S+)>")
 
-get_relay_and_dsn_re = re.compile("(\w+\s+\d+\s[0-9:].+)\s([\w\.]+) "
+destination_and_from_re = re.compile("(\w+\s+\d+\s[0-9:].+)\s([\w\.]+) "
                                   "postfix\/\S+\[\d+\]: (\S+): to=<(\S+)>.+ "
                                   "relay=(\S+),.+ status=(\S+) \((.+)\)")
-queue_id_removed_re = re.compile("\w+\s+\d+\s[0-9:]+\s[\w\.].+ "
+queue_removed_re = re.compile("\w+\s+\d+\s[0-9:]+\s[\w\.].+ "
                                  "postfix\/qmgr\[\d+\]: (\S+): removed$")
-get_message_id_re = re.compile("\w+\s+\d+\s[0-9:]+\s[\w\.].+ "
+messaged_id_re = re.compile("\w+\s+\d+\s[0-9:]+\s[\w\.].+ "
                                "postfix\/cleanup\[\d+\]: (\S+): "
                                "message-id=<(\S+)>")
 get_from_re = re.compile("\w+\s+\d+\s[0-9:]+\s[\w\.].+ "
                          "postfix\/qmgr\[\d+\]: "
                          "(\S+): from=<(\S+)>, size=(\d+)")
-sender_address_rejected_re = re.compile("(\w+\s+\d+\s[0-9:].+)\s([\w\.]+) "
+rejected_sender_re = re.compile("(\w+\s+\d+\s[0-9:].+)\s([\w\.]+) "
                                         "postfix\/smtpd\[\d+\]: NOQUEUE: "
                                         "reject: RCPT from \S+\[(\S+)\]: "
                                         "(\d\d\d \d\.\d\.\d .+ from=<(\S+)> "
                                         "to=<(\S+)> .+)")
-relay_ip_re = re.compile("\S+\[(\S+)\]")
 
-get_auth_sender_re = re.compile("\w+\s+\d+\s[0-9:].+\s[\w\.]+ "
+auth_sender_re = re.compile("\w+\s+\d+\s[0-9:].+\s[\w\.]+ "
                                 "postfix\/cleanup\[\d+\]: (\S+): "
                                 ".+\(Authenticated sender: (\S+)\)\?\?")
 
-get_amavis_spam_re = re.compile("(\w+\s+\d+\s[0-9:].+)\s[\w\.]+ "
+amavis_spam_re = re.compile("(\w+\s+\d+\s[0-9:].+)\s[\w\.]+ "
                                 "amavis\[\d+\]: \(\S+\) (\S+) (\S+) "
                                 "{(\w+),(\w+)}, \[\S+\].*\[(\S+)\] "
                                 "<(\S+)> -> <(\S+)>, .+ Message-ID: "
                                 "<(\S+)>, .+, Hits: (\S+), size: (\S+), "
                                 "queued_as: (\S+), ")
 
-get_successful_sasl_auth_re = re.compile("(\w+\s+\d+\s[0-9:].+)\s[\w\.]+ "
+successful_sasl_re = re.compile("(\w+\s+\d+\s[0-9:].+)\s[\w\.]+ "
                                          "postfix\/smtpd\[\d+\]: (\S+): "
                                          "client=(\S+)\[(\S+)\], "
                                          "sasl_method=(\S+), "
                                          "sasl_username=(\S+)")
 
-get_failed_sasl_auth_re = re.compile(
+failed_sasl_re = re.compile(
     "(\w+\s+\d+\s[0-9:].+)\s[\w\.]+ postfix\/smtpd\[\d+\]: "
     "(\S+): (\S+)\[(\S+)\]: SASL (.+): (.+)")
+
+relay_ip_re = re.compile("\S+\[(\S+)\]")
--- parser.py
+++ parser.py
@@ -12,7 +12,7 @@ class MailParser(object):
     def __init__(self):
         self.mobjects = []
 
-    def get_relay_info(self, relay):
+    def dest_ip_info(self, relay):
         m = relay_ip_re.match(relay)
         if m:
             return m.group(1)
@@ -28,29 +28,29 @@ class MailParser(object):
         return o[0]
 
     def get_auth_sender(self, line):
-        m = get_auth_sender_re.match(line)
+        m = auth_sender_re.match(line)
         if m:
             o = self.find_local_id(m.group(1))
             o.userID = m.group(2)
 
     def get_client_info(self, line):
-        m = get_originating_ip_re.match(line)
+        m = sending_ip_re.match(line)
         if m:
             o = self.find_local_id(m.group(1))
             o.sending_ip = m.group(2)
 
     def get_subject(self, line):
-        m = get_subject_and_addrs_re.match(line)
+        m = subject_and_from_re.match(line)
         if m:
             o = self.find_local_id(m.group(1))
             o.subject = m.group(2)
             o.mailfrom = m.group(3)
 
     def get_dsn(self, line):
-        m = get_relay_and_dsn_re.match(line)
+        m = destination_and_from_re.match(line)
         if m:
             o = self.find_local_id(m.group(3))
-            o.destinationIP = self.get_relay_info(m.group(5))
+            o.destinationIP = self.dest_ip_info(m.group(5))
             o.result = m.group(6)
             o.result_detail = m.group(7)
             o.to = m.group(4)
@@ -64,7 +64,7 @@ class MailParser(object):
             return o
 
     def get_message_id(self, line):
-        m = get_message_id_re.match(line)
+        m = messaged_id_re.match(line)
         if m:
             o = self.find_local_id(m.group(1))
             o.message_id = m.group(2)
@@ -77,7 +77,7 @@ class MailParser(object):
             o.size = m.group(3)
 
     def sender_addr_rejected(self, line):
-        m = sender_address_rejected_re.match(line)
+        m = rejected_sender_re.match(line)
         if m:
             o = MailObject(None)
             o.sending_ip = m.group(3)
@@ -91,13 +91,13 @@ class MailParser(object):
             return o
 
     def get_removed(self, line):
-        m = queue_id_removed_re.match(line)
+        m = queue_removed_re.match(line)
         if m:
             o = self.find_local_id(m.group(1))
             self.mobjects.remove(o)
 
     def get_amavis_spam(self, line):
-        m = get_amavis_spam_re.match(line)
+        m = amavis_spam_re.match(line)
         if m:
             o = MailObject(m.group(12))
             o.timestamp = m.group(1)
@@ -115,7 +115,7 @@ class MailParser(object):
             return o
 
     def get_successful_sasl_auth(self, line):
-        m = get_successful_sasl_auth_re.match(line)
+        m = successful_sasl_re.match(line)
         if m:
             o = MailObject(m.group(2))
             o.timestamp = m.group(1)
@@ -126,7 +126,7 @@ class MailParser(object):
             return o
 
     def get_failed_sasl_auth(self, line):
-        m = get_failed_sasl_auth_re.match(line)
+        m = failed_sasl_re.match(line)
         if m:
             o = MailObject(None)
             o.timestamp = m.group(1)
